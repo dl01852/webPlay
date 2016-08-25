60712a4173f6d820cf8de5f886ebf44481326d95
DSLewis22 <dl01852@georgiasouthern.edu>
2016-08-25 15:49:23 -0400

    updated various sites

--- flaskPlay.py
+++ flaskPlay.py
@@ -7,25 +7,31 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/lists')
 def lists():
     return render_template('listPlay.html')
+
 
 @app.route('/tables')
 def tables():
     return render_template('tablePlay.html')
 
+
 @app.route('/form')
 def form():
     return render_template('Form.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/solution15')
 def solution15():
     return render_template('solution15.html')
+
 
 @app.route('/solution16')
 def solution16():
